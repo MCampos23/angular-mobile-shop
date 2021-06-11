a21e62af9fe63ce3921c8efee8d28a2f31af9c26
DESKTOP-FBH0P1R\34605 <camposvmiguelangel@gmail.com>
2021-06-11 13:24:01 +0200

    delete useless folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
-import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartService } from './cart.service';
 import { CartComponent } from './cart/cart.component';
 import { HttpClientModule } from '@angular/common/http';
-import { ShippingComponent } from './shipping/shipping.component'
 
 @NgModule({
   imports: [
@@ -22,25 +20,19 @@ import { ShippingComponent } from './shipping/shipping.component'
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
       { path: 'cart', component: CartComponent },
-      { path: 'shipping', component: ShippingComponent }
+      
     ])
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
     ProductListComponent,
-    ProductAlertsComponent,
     ProductDetailsComponent,
     CartComponent,
-    ShippingComponent
+   
   ],
   bootstrap: [AppComponent],
   providers: [CartService]
 })
 export class AppModule {}
 
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,25 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { CartService } from '../cart.service'
+import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.css']
 })
 export class TopBarComponent {
-  itemsLength = this.cartService.getItemsLength()
-
-  constructor(
-    private cartService: CartService
-  ){}
-
-  ngOnInit(){
-    
-  }
+ 
 }
 
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
